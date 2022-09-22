9a90f6c2db4819ade02d82c4240a8e87a18170f3
FLpython <flanker02@gmail.com>
2022-09-22 18:05:22 +0200

    this are the dags where one is triggering another

--- dags/dag_1.py
+++ dags/dag_1.py
@@ -6,7 +6,7 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 
 
 with DAG(
-        dag_id='1_create_db_dag',
+        dag_id='1_insert_values',
         # default_args=default_args,
         description='creates_table',
         start_date=datetime(2022, 9, 22),
@@ -14,12 +14,13 @@ with DAG(
         schedule_interval='@once',
         tags=['dag_1']
 ) as dag:
-    task1 = PostgresOperator(
-        task_id='insert_values', postgres_conn_id='my_db', autocommit=True,
-        sql=sql_dict['insert_values'])
-
     trigger_target = TriggerDagRunOperator(
         task_id='trigger_dag2',
         trigger_dag_id='2_select_values',
-        reset_dag_run=True
+        reset_dag_run=True,
+        wait_for_completion=True
     )
+
+    task1 = PostgresOperator(
+        task_id='insert_values', postgres_conn_id='my_db', autocommit=True,
+        sql=sql_dict['insert_values'])
